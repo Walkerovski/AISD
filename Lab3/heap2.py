5f984419de93336ea617c5c7a255ff1a66ee3d23
Piotr Kosmala <piotr.kosmala2stud@pw.edu.pl>
2021-05-09 22:28:33 +0200

    Kopiec 3-arny, wypisywanie do xml, testy

--- Lab3/heap2.py
+++ Lab3/heap2.py
@@ -1,3 +1,6 @@
+from xml.dom import minidom
+
+
 def insert(array, value):
     array.append(value)
     current = len(array) - 1
@@ -23,6 +26,30 @@ def heapify(array, index):
                 heapify(array, 2*index + 1)
 
 
+def printXML(root, fileName):
+    doc = minidom.Document()
+    node = doc.createElement("root")
+    node.setAttribute('value', str(root[1]))
+    doc.appendChild(node)
+    if 2 < len(root):
+        printNode(2, node, doc)
+    if 3 < len(root):
+        printNode(3, node, doc)
+    xml_str = doc.toprettyxml(indent='\t')
+    with open(fileName, "w") as f:
+        f.write(xml_str)
+
+
+def printNode(node, parent, doc):
+    child = doc.createElement("child")
+    child.setAttribute('value', str(heap[node]))
+    parent.appendChild(child)
+    if 2*node < len(heap):
+        printNode(2*node, child, doc)
+    if 2*node + 1 < len(heap):
+        printNode(2*node + 1, child, doc)
+
+
 heap = [-1]
 insert(heap, 5)
 insert(heap, 3)
@@ -33,6 +60,7 @@ insert(heap, 19)
 insert(heap, 6)
 insert(heap, 22)
 insert(heap, 9)
+printXML(heap, "heap2.xml")
 print(heap)
 remove(heap)
 print(heap)
--- Lab3/testing.py
+++ Lab3/testing.py
@@ -1,4 +1,5 @@
 import heap2
+import heap3
 import random
 import time
 import matplotlib.pyplot as plt
@@ -32,19 +33,49 @@ def testRemove2():
     return times
 
 
+def testInsertion3():
+    times = []
+    for i in range(10):
+        heap = [-1]
+        start = time.perf_counter()
+        for j in range(i*10000):
+            heap3.insert(heap, j)
+        stop = time.perf_counter()
+        times.append(stop-start)
+    return times
+
+
+def testRemove3():
+    times = []
+    for i in range(10):
+        heap = [-1]
+        for j in numbers:
+            heap2.insert(heap, j)
+        start = time.perf_counter()
+        for j in range(i*10000):
+            heap3.remove(heap)
+        stop = time.perf_counter()
+        times.append(stop-start)
+    return times
+
+
 if __name__ == "__main__":
     insertionTimes2 = testInsertion2()
     removeTimes2 = testRemove2()
+    insertionTimes3 = testInsertion3()
+    removeTimes3 = testRemove3()
     plt.plot(insertionTimes2)
+    plt.plot(insertionTimes3)
     plt.title("Tworzenie kopca n elementów")
     plt.xlabel("Liczba elementów (w 10 tys.)")
     plt.ylabel("Czas (w sekundach)")
-    plt.legend(["Kopiec 2-arny"])
+    plt.legend(["Kopiec 2-arny", "Kopiec 3-arny"])
     plt.savefig("insertionPlot.png")
     plt.clf()
     plt.plot(removeTimes2)
+    plt.plot(removeTimes3)
     plt.title("Usuwanie n elementów z kopca 300000 elementów")
     plt.xlabel("Liczba usuwanych elementów (w 10 tys.)")
     plt.ylabel("Czas (w sekundach)")
-    plt.legend(["Kopiec 2-arny"])
+    plt.legend(["Kopiec 2-arny", "Kopiec 3-arny"])
     plt.savefig("removePlot.png")
